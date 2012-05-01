9a869ebda338dc5e03fcd8f05a7eb6562133c180
Kai Sellgren <kaisellgren@gmail.com>
2012-05-01 17:27:42 +0300

    Added a reusable DelegateCommand class.

---
+++ DelegateCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+public class DelegateCommand : ICommand
+{
+    private readonly Action<object> execute;
+    private readonly Predicate<object> canExecute;
+
+    public DelegateCommand(Action<object> execute)
+        : this(execute, null)
+    { }
+
+    public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+    {
+        if (execute == null)
+            throw new ArgumentNullException("execute");
+
+        this.execute = execute;
+        this.canExecute = canExecute;
+    }
+
+    #region ICommand Members
+
+    [DebuggerStepThrough]
+    public bool CanExecute(object parameter)
+    {
+        return canExecute == null ? true : canExecute(parameter);
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+
+    public void Execute(object parameter)
+    {
+        execute(parameter);
+    }
+
+    #endregion
+}
