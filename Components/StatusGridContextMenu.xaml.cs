1a5aab784f513d4dd75135408cc92525a4a08eda
Kai Sellgren <kaisellgren@gmail.com>
2012-05-06 20:38:38 +0300

    Improved status grid context menu.

---
+++ Components/StatusGridContextMenu.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GG.Components
+{
+    public partial class StatusGridContextMenu : ResourceDictionary
+    {
+        public StatusGridContextMenu()
+        {
+            InitializeComponent();
+        }  
+
+        void OnContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            Console.WriteLine("here i am!");
+        }
+
+        private void ContextMenu_ContextMenuClosing_1(object sender, ContextMenuEventArgs e)
+        {
+            Console.WriteLine("here i am2!");
+        }
+    }
+}
