10e95354b9e4a129b35c42ab5861e81fb16b9f35
Kai Sellgren <kaisellgren@gmail.com>
2012-05-17 19:56:01 +0300

    Added coloring for status grid group headers.

---
+++ Converters/StatusGridGroupToColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace GG.Converters
+{
+    /// <summary>
+    /// Converts LibGit2Sharp.Status into a single character string.
+    /// </summary>
+    class StatusGridGroupToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((string) value) == "Staged" ? new SolidColorBrush(Color.FromRgb(76, 120, 0)) : new SolidColorBrush(Color.FromRgb(120, 37, 0));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Brushes.White;
+        }
+    }
+}
