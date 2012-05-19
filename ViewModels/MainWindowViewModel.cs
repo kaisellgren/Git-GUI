a0530b32c1ac362e2d8147d9282061e76ee7ebab
Kai Sellgren <kaisellgren@gmail.com>
2012-05-19 17:04:07 +0300

    Added support for Ctrl+W and Ctrl+T (tabs).

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using GG.Libraries;
 using GG.ViewModels;
 
 namespace GG
@@ -15,6 +18,9 @@ namespace GG
         {
             RepositoryViewModels = new ObservableCollection<RepositoryViewModel> { };
             RecentRepositories = new ObservableCollection<RepositoryViewModel> { };
+
+            CreateTabCommand = new DelegateCommand(CreateTab);
+            CloseTabCommand = new DelegateCommand(CloseTab);
         }
 
         public void Load()
@@ -38,5 +44,44 @@ namespace GG
             // Add some "recent repositories".
             RecentRepositories.Add(repo);
         }
+
+        #region Commands.
+
+        public DelegateCommand CreateTabCommand { get; private set; }
+        public DelegateCommand CloseTabCommand { get; private set; }
+
+        /// <summary>
+        /// Closes the current tab.
+        /// </summary>
+        /// <param name="action"></param>
+        private void CloseTab(object action)
+        {
+            TabControl tabControl = UIHelper.FindChild<TabControl>(Application.Current.MainWindow, "RepositoryTabs");
+            ObservableCollection<RepositoryViewModel> repositories = tabControl.ItemsSource as ObservableCollection<RepositoryViewModel>;
+            repositories.Remove(tabControl.SelectedContent as RepositoryViewModel);
+        }
+
+        /// <summary>
+        /// Creates a new tab.
+        /// </summary>
+        /// <param name="action"></param>
+        private void CreateTab(object action)
+        {
+            TabControl tabControl = UIHelper.FindChild<TabControl>(Application.Current.MainWindow, "RepositoryTabs");
+            MainWindowViewModel mainWindowViewModel = Application.Current.MainWindow.DataContext as MainWindowViewModel;
+
+            RepositoryViewModel repository = new RepositoryViewModel
+            {
+                Name = "New Tab",
+                NotOpened = true,
+                RepositoryFullPath = ""
+            };
+
+            mainWindowViewModel.RepositoryViewModels.Add(repository);
+
+            tabControl.SelectedItem = repository;
+        }
+
+        #endregion
     }
 }
