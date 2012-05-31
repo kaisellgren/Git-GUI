8526d6172276d40033cac53999a9c219685b6197
Kai Sellgren <kaisellgren@gmail.com>
2012-05-31 19:53:57 +0300

    Added highlights to changeset history for HEAD.

---
+++ Converters/IsHeadToFontWeightConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace GG.Converters
+{
+    /// <summary>
+    /// Converts IsHead status into a font weight.
+    /// </summary>
+    class IsHeadToFontWeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value ? FontWeights.Bold : FontWeights.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return false;
+        }
+    }
+}
---
+++ Converters/IsHeadToTextDecorationsConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace GG.Converters
+{
+    /// <summary>
+    /// Converts IsHead status into text decorations.
+    /// </summary>
+    class IsHeadToTextDecorationsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value ? TextDecorations.Underline : new TextDecorationCollection();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return false;
+        }
+    }
+}
