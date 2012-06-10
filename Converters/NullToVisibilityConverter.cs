2eaae942639389548a9a4be114c156351eb6f52f
Kai Sellgren <kaisellgren@gmail.com>
2012-06-10 17:13:45 +0300

    A few configuration changes.

---
+++ Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GG.Converters
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        #region Implementation of IValueConverter
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
