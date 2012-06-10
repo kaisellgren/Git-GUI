948a44e1f2817b974a9466f423a948d8a2a64dad
Kai Sellgren <kaisellgren@gmail.com>
2012-06-10 17:06:07 +0300

    Added two new controls.

---
+++ UserControls/MenuButton.cs
@@ -0,0 +1,24 @@
+﻿namespace GG.UserControls
+{
+    /// <summary>
+    /// Implements a "menu button" for WPF.
+    /// </summary>
+    public class MenuButton : SplitButton
+    {
+        /// <summary>
+        /// Initializes a new instance of the MenuButton class.
+        /// </summary>
+        public MenuButton()
+        {
+            DefaultStyleKey = typeof(MenuButton);
+        }
+
+        /// <summary>
+        /// Called when the button is clicked.
+        /// </summary>
+        protected override void OnClick()
+        {
+            OpenButtonMenu();
+        }
+    }
+}
