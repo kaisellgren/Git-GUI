de6d20ba481ddcbbc8556f8b2dd568382fe09629
Kai Sellgren <kaisellgren@gmail.com>
2012-06-22 18:48:05 +0300

    Improved UIHelper.

--- Libraries/UIHelper.cs
+++ Libraries/UIHelper.cs
@@ -7,18 +7,75 @@ namespace GG.Libraries
     class UIHelper
     {
         /// <summary>
-        /// Finds a Child of a given item in the visual tree. 
+        /// Returns the first ancester of specified type
         /// </summary>
-        /// <param name="parent">A direct parent of the queried item.</param>
-        /// <typeparam name="T">The type of the queried item.</typeparam>
-        /// <param name="childName">x:Name or Name of child. </param>
-        /// <returns>The first parent item that matches the submitted type parameter. 
-        /// If not matching item can be found, 
-        /// a null parent is being returned.</returns>
-        public static T FindChild<T>(DependencyObject parent, string childName)
-           where T : DependencyObject
+        public static T FindAncestor<T>(DependencyObject current)
+        where T : DependencyObject
         {
-            // Confirm parent and childName are valid. 
+            current = VisualTreeHelper.GetParent(current);
+
+            while (current != null)
+            {
+                if (current is T)
+                {
+                    return (T) current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            };
+            return null;
+        }
+
+        /// <summary>
+        /// Returns a specific ancester of an object
+        /// </summary>
+        public static T FindAncestor<T>(DependencyObject current, T lookupItem)
+        where T : DependencyObject
+        {
+            while (current != null)
+            {
+                if (current is T && current == lookupItem)
+                {
+                    return (T) current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            };
+            return null;
+        }
+
+        /// <summary>
+        /// Finds an ancestor object by name and type
+        /// </summary>
+        public static T FindAncestor<T>(DependencyObject current, string parentName)
+        where T : DependencyObject
+        {
+            while (current != null)
+            {
+                if (!string.IsNullOrEmpty(parentName))
+                {
+                    var frameworkElement = current as FrameworkElement;
+                    if (current is T && frameworkElement != null && frameworkElement.Name == parentName)
+                    {
+                        return (T) current;
+                    }
+                }
+                else if (current is T)
+                {
+                    return (T) current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            };
+
+            return null;
+
+        }
+
+        /// <summary>
+        /// Looks for a child control within a parent by name
+        /// </summary>
+        public static T FindChild<T>(DependencyObject parent, string childName)
+        where T : DependencyObject
+        {
+            // Confirm parent and childName are valid.
             if (parent == null) return null;
 
             T foundChild = null;
@@ -34,7 +91,7 @@ namespace GG.Libraries
                     // recursively drill down the tree
                     foundChild = FindChild<T>(child, childName);
 
-                    // If the child is found, break so we do not overwrite the found child. 
+                    // If the child is found, break so we do not overwrite the found child.
                     if (foundChild != null) break;
                 }
                 else if (!string.IsNullOrEmpty(childName))
@@ -47,6 +104,14 @@ namespace GG.Libraries
                         foundChild = (T) child;
                         break;
                     }
+                    else
+                    {
+                        // recursively drill down the tree
+                        foundChild = FindChild<T>(child, childName);
+
+                        // If the child is found, break so we do not overwrite the found child.
+                        if (foundChild != null) break;
+                    }
                 }
                 else
                 {
@@ -56,6 +121,41 @@ namespace GG.Libraries
                 }
             }
 
+            return foundChild;
+        }
+
+        /// <summary>
+        /// Looks for a child control within a parent by type
+        /// </summary>
+        public static T FindChild<T>(DependencyObject parent)
+            where T : DependencyObject
+        {
+            // Confirm parent is valid.
+            if (parent == null) return null;
+
+            T foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                // If the child is not of the request child type child
+                T childType = child as T;
+                if (childType == null)
+                {
+                    // recursively drill down the tree
+                    foundChild = FindChild<T>(child);
+
+                    // If the child is found, break so we do not overwrite the found child.
+                    if (foundChild != null) break;
+                }
+                else
+                {
+                    // child element found.
+                    foundChild = (T) child;
+                    break;
+                }
+            }
             return foundChild;
         }
     }
