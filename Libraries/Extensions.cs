f7cd47f5f4eb731cde448d4001e6aac9936dacda
Kai Sellgren <kaisellgren@gmail.com>
2012-06-22 23:43:41 +0300

    Added a cloning extension.

--- Libraries/Extensions.cs
+++ Libraries/Extensions.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace GG
 {
@@ -33,6 +36,35 @@ namespace GG
         public static string RemoveLineBreaks(this string str)
         {
             return str.Replace("\n", "").Replace("\r", "");
+        }
+
+        /// <summary>
+        /// Perform a deep Copy of the object.
+        /// </summary>
+        /// <typeparam name="T">The type of object being copied.</typeparam>
+        /// <param name="source">The object instance to copy.</param>
+        /// <returns>The copied object.</returns>
+        public static T Clone<T>(this T source)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("The type must be serializable.", "source");
+            }
+
+            // Don't serialize a null object, simply return the default for that object
+            if (Object.ReferenceEquals(source, null))
+            {
+                return default(T);
+            }
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            using (stream)
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T) formatter.Deserialize(stream);
+            }
         }
     }
 }
