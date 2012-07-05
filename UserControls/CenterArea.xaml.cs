22337bda150423033969410377b860b42729be32
Kai Sellgren <kaisellgren@gmail.com>
2012-07-05 17:14:25 +0300

    GridSplitter snaps to changeset history grid.

---
+++ UserControls/CenterArea.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace GG.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CenterArea.xaml
+    /// </summary>
+    public partial class CenterArea : UserControl
+    {
+        public CenterArea()
+        {
+            InitializeComponent();
+        }
+
+        private void GridSplitterDragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            // We want the grid splitter to snap in grid of 24 units.
+            var excess = (int) ChangesetHistoryRowDefinition.Height.Value % 24;
+
+            if (excess == 0)
+                return;
+
+            ChangesetHistoryRowDefinition.Height = new GridLength(ChangesetHistoryRowDefinition.Height.Value - excess);
+        }
+    }
+}
