33b3b0d9acb4a33bdce10366eddf6a63e573e939
Kai Sellgren <kaisellgren@gmail.com>
2012-07-05 17:57:42 +0300

    Added global exception handler.

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using GG.UserControls.Dialogs;
 
 namespace GG
 {
@@ -14,11 +15,30 @@ namespace GG
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            MainWindow mainWindow = new MainWindow();
-            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
-            mainWindow.DataContext = mainWindowViewModel;
+            var mainWindow = new MainWindow
+            {
+                DataContext = new MainWindowViewModel()
+            };
+
             base.MainWindow = mainWindow;
+
+            // Set up a global exception handler.
+            //AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+
             mainWindow.Show();
+        }
+
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            var dialog = new PromptDialog
+            {
+                Title = "Error occured",
+                Message = unhandledExceptionEventArgs.ExceptionObject.ToString()
+            };
+
+            dialog.ShowDialog();
+
+            Environment.Exit(1);
         }
     }
 }
