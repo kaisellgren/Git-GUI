6a74c009c1566e70e9146f62a6325edb18d0e147
Kai Sellgren <kaisellgren@gmail.com>
2012-07-24 00:53:51 +0300

    Added UTF-8 headers.

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ var templater = require('../lib/templater.js');
 module.exports = {
     index: function() {
         this.res.writeHead(200, {
-            'Content-Type': 'text/html'
+            'Content-Type': 'text/html; charset=utf-8'
         });
 
         templater.render('layout.ejs', {}, function(output) {
